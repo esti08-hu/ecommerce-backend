005703dc450b2ff76c9817f34533d97127cc1812
esti08-hu <estioame@gmail.com>
2025-01-27 09:53:30 +0300

    add initial service and controller files for core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AdminModule } from './admin/admin.module';
+import { ContentModule } from './content/content.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ProductsModule, OrdersModule, CartModule, PaymentsModule, ReviewsModule, NotificationsModule, AdminModule, ContentModule],
   controllers: [AppController],
   providers: [AppService],
 })
