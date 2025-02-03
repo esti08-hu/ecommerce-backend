16c502930f00e68c4d3dedb85aa82530568383e3
esti08-hu <estioame@gmail.com>
2025-02-03 22:32:51 +0300

    updated invoice

--- src/orders/invoices/createInvoice.ts
+++ src/orders/invoices/createInvoice.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import PDFDocument from 'pdfkit';
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
 
 export function createInvoice(invoice, path) {
   const doc = new PDFDocument({ margin: 50 });
@@ -17,18 +17,18 @@ module.exports = {
 
 function generateHeader(doc, invoice) {
   doc
-    .image('logo.png', 50, 45, { width: 50 })
+    // .image('logo.jpg', 50, 45, { width: 50 })
     .fillColor('#444444')
     .fontSize(20)
     .text(invoice.companyName, 110, 57)
     .fontSize(10)
-    .text(invoice.companyAddress.street, 200, 65, { align: 'right' })
+    .text(invoice.companyAddress?.street, 200, 65, { align: 'right' })
     .text(
-      invoice.companyAddress.city +
+      invoice.companyAddress?.city +
         ', ' +
-        invoice.companyAddress.state +
+        invoice.companyAddress?.state +
         ', ' +
-        invoice.companyAddress.zip,
+        invoice.companyAddress?.zip,
       200,
       80,
       { align: 'right' },
